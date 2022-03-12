e03c67983a989d7f3ede1b23da566ba7461fdbd3
Henzii <80477931+Henzii@users.noreply.github.com>
2022-03-12 16:38:15 +0200

    Push notifikaatioiden tikettien tarkastu 30sek paahan lahetyksesta

--- services/pushNotificationsService.ts
+++ services/pushNotificationsService.ts
@@ -37,7 +37,7 @@ const sendPushNotifications = async (pushTokens: ExpoPushToken[], message: PushM
     // Tarkastetaan kuitit 30sek päästä virheiden varalta
     setTimeout(() => {
         checkReceipts(ticketsAndTokens);
-    }, 3000);
+    }, 30000);
 };
 const checkReceipts = async (tickets: ((ExpoPushTicket) & { token?: string })[]) => {
     console.info('Tarkastetaan tiketit');
