37cc791a2ffce4dea800f7e1ecf10366f8078428
Henzii <80477931+Henzii@users.noreply.github.com>
2022-03-17 15:49:18 +0200

    Course modeliin koordinaatit mukaan, default 0,0

--- models/Course.ts
+++ models/Course.ts
@@ -7,6 +7,16 @@ const skeema = new mongoose.Schema({
         minlength: 3,
         unique: true,
     },
+    location: {
+        type: {
+            type: String,
+            default: 'Point',
+        },
+        coordinates: {
+            type: [Number],
+            default: [0.0, 0.0]
+        },
+    },
     layouts: [
         {
             name: {
