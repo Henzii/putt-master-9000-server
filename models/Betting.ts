30c8f57d34e901e8ef5addd30c3e4081f8f08457
Karlenius Henry <henry.karlenius@veikkaus.fi>
2025-11-24 08:31:21 +0200

    betting

---
+++ models/Betting.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+export const pool = new mongoose.Schema({
+  status: String,
+  type: String,
+  bets: [{
+    totalStake: Number,
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User'
+    },
+    selections: [Number],
+    lines: [{
+      selections: [Number],
+      stake: Number
+    }]
+  }],
+  result: {
+    winningSelection: Number
+  }
+});
